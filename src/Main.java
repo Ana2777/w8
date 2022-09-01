9ad4ab1f8bbd20de314a9960d5b270395de273f1
Ana <9025015386@gmail.ru>
2022-09-01 21:10:54 +0500

    4

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,9 @@
-
 public class Main {
-    public static void whatYear(int year) {
-        if (year % 100 >= 0 || year % 400 == 0 || year % 4 == 0) {
-            System.out.print(" - год является високостным");
+    public static void whatYear (int year) {
+        if (year % 100 == 0 || year % 400 == 0 || year % 100 > 0 && year % 4 == 0 ) {
+            System.out.print("год является високостным");
         } else {
-            System.out.print(" - год не является високостным");
+            System.out.print("год не является високостным");
         }
     }
     public static void clientIos (int currentYear, int clientOs) {
@@ -17,26 +16,21 @@ public class Main {
         } else if (clientOs == 1 && currentYear < 2015) {
             System.out.println("Установите облегченную версию для Android");
         }
-        return;
     }
-    public static String determinationOfDeliveryDays (int distance) {
+    public static void determinationOfDeliveryDays (int distance) {
         int deliveryTime = 1;
-        System.out.println(distance <= 20 ? "Потребуется дней" + deliveryTime : distance <= 60 && deliveryTime >=20 ? "Потребуется дней" + (deliveryTime+1) : deliveryTime <= 100 && deliveryTime >= 60 ? "Потребуется дней" + (deliveryTime + 2));
+        return (distance <= 20 ? "Потребуется дней" + deliveryTime : distance <= 60 && deliveryTime >= 20 ? "Потребуется дней" + (deliveryTime + 1) : deliveryTime <= 100 && deliveryTime >= 60 ? "Потребуется дней" + (deliveryTime + 2));
     }
-    public static void main (String[] args) {
-    //Задание 1
-    int year = 2019;
-    whatYear();
-        // Задание 2
-        int currentYear = 2022;
-        int сlientOs = 1;
-        clientIos();
-
-// Задание 3
-        int distance = 50;
-        determinationOfDeliveryDays();
-
-
+        public static void main (String[] args) {
+        //Задание 1
+whatYear(2020);
+// Задание 2
+        //int currentYear = 2022;
+       // int сlientOs = 1;
+        clientIos(2022, 1);
+        // Задание 3
+        //int distance = 50;
+        determinationOfDeliveryDays(50);
 
 
     }
